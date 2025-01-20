cd5d80f9b0d723a6dc648f17d0981bcb345b53dc
angeben <anbego99@gmail.com>
2025-01-20 13:47:12 +0100

    feat: add project with TS
    Guitar shop project with TypeScript

---
+++ guitar-shop-ts/src/App.tsx
@@ -0,0 +1,44 @@
+import Header from "./components/Header.js"
+import Footer from "./components/Footer.js"
+import Guitar from "./components/Guitar.js"
+import { useCart } from './hooks/useCart.js'
+
+
+function App() {
+
+  const {data, cart, addToCart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart,
+    cartIsEmpty, cartTotal
+  } = useCart()
+
+  return (
+    <>  
+    <Header 
+      cart = {cart}
+      removeFromCart = {removeFromCart}
+      increaseQuantity = {increaseQuantity}
+      decreaseQuantity = {decreaseQuantity}
+      clearCart = {clearCart}
+      cartIsEmpty = {cartIsEmpty}
+      cartTotal = {cartTotal}
+    />  
+    <main className="container-xl mt-5">
+        <h2 className="text-center">Our Guitar Collection</h2>
+        
+        <div className="row mt-5">
+            {data.map((guitar) => (
+                 <Guitar
+                    // Unique value necessary for iterations
+                    key = {guitar.id}
+                    guitar = {guitar}
+                    addToCart = {addToCart}
+                 />
+            ))}
+        </div> 
+        
+    </main>
+    <Footer />
+    </>
+  )
+}
+
+export default App
