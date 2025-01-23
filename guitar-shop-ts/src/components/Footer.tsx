dc6bf6b20284c5101fee038aabc98270f608a503
angeben <anbego99@gmail.com>
2025-01-23 13:35:25 +0100

    Update Footer.tsx
    add linkedIn and github links

--- guitar-shop-ts/src/components/Footer.tsx
+++ guitar-shop-ts/src/components/Footer.tsx
@@ -2,9 +2,19 @@
 export default function Footer(){
     return(
         <footer className="bg-dark mt-5 py-5">
-        <div className="container-xl">
-            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - All rights reserved</p>
-        </div>
-    </footer>
+            <div className="container-xl">
+                <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - All rights reserved</p>
+                <p className="text-white text-center fs-2 mt-4 m-md-0">
+                    <a href="https://www.linkedin.com/in/angel-benitez-gomez/">
+                        Check out my LinkedIn
+                    </a>
+                </p>
+                <p className="text-white text-center fs-2 mt-4 m-md-0">
+                    <a href="https://github.com/angeben">
+                        Check out my Github
+                    </a>
+                </p>
+            </div>
+        </footer>
     )
 }
